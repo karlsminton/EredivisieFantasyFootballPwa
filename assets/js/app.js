bd8ab03e09643e6f4457ab88085039e55d2915b1
karlsminton <ksm1nn0@gmail>
2022-05-26 21:37:44 +0100

    begin implementing vue routing and components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,37 @@
-const container = document.querySelector('main')
-
-const data = [
-  {
-    name: 'kitten',
-    image: 'assets/media/images/large-kitten.jpg'
-  }
-]
-
-const renderData = () => {
-  let html = ''
-  data.forEach(
-    ({name, image}) =>
-    (html += `
-        <div class="item">
-          <img class="item__image" src=${image} />
-          <p>${name}</>
-        </div>
-      `)
-  )
-  container.innerHTML = html
-}
-
-document.addEventListener('DOMContentLoaded', renderData)
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './App.vue'
+import Post from './components/Post.vue'
+import Index from './components/Index.vue'
+Vue.use(Router)
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker
       .register('/serviceWorker.js')
-      .then(res => console.log('service worker registered'))
-      .catch(error => console.log('service worker not registered', error))
+      .then(res => console.log('service worker registered.'))
+      .catch(error => console.log('service worker failed to register.', error))
   })
 }
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Index
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Post,
+      props: true,
+    },
+  ]
+})
+
+new Vue({
+  el: 'main',
+  render: h => h(App),
+  router
+})
