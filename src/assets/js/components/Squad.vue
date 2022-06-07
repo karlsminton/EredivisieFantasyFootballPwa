0ef9a4a38e2adc8566cd7e041d7917c7eb268fc2
karlsminton <ksm1nn0@gmail>
2022-06-07 22:17:24 +0100

    Change id to use what-will-be api data. disallow players being assigned to incorrect positions.

--- src/assets/js/components/Squad.vue
+++ src/assets/js/components/Squad.vue
@@ -36,7 +36,7 @@
             draggable="true"
             v-on:dragstart="dragStart"
             v-on:drag="dragging"
-            :id="player.name"
+            :id="`player-${player.id}`"
         >
             <img :src="player.icon" draggable="false"/>
             <p class="name">{{ player.name }}</p>
@@ -58,56 +58,67 @@ export default {
       /* exact implementation likely to change */
       bench: [
         {
+          id: 1,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Onana',
           position: 'goalkeeper',
         },
         {
+          id: 2,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Tagliafico',
           position: 'defender',
         },
         {
+          id: 3,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Blind',
           position: 'defender',
         },
         {
+          id: 4,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Schuurs',
           position: 'defender',
         },
         {
+          id: 5,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Mazraoui',
           position: 'defender',
         },
         {
+          id: 6,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Gravenberch',
           position: 'midfielder',
         },
         {
+          id: 7,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Klaassen',
           position: 'midfielder',
         },
         {
+          id: 8,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Tadić',
           position: 'midfielder',
         },
         {
+          id: 9,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Berghuis',
           position: 'midfielder',
         },
         {
+          id: 10,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Antony',
           position: 'midfielder',
         },
         {
+          id: 11,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Haller',
           position: 'offence',
@@ -115,21 +126,25 @@ export default {
 
         /** Actual bench **/
         {
+          id: 12,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Timber',
           position: 'defender',
         },
         {
+          id: 13,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Rensch',
           position: 'defender',
         },
         {
+          id: 14,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Brobbey',
           position: 'offence',
         },
         {
+          id: 15,
           icon: 'http://phpdocker.local/ajax.png',
           name: 'Kudus',
           position: 'midfielder',
@@ -138,18 +153,26 @@ export default {
     }
   },
   methods: {
-    dragStart:function(event)  {
-      event.dataTransfer.setData("Text", event.target.id);
+    dragStart(event) {
+      event.dataTransfer.setData("Text", event.target.id)
     },
-    dragging:function(event) {},
-    allowDrop:function(event) {
-      event.preventDefault();
+    dragging(event) {},
+    allowDrop(event) {
+      event.preventDefault()
     },
-    drop:function(event) {
-      event.preventDefault();
-      var data = event.dataTransfer.getData("Text");
-      console.log(data)
-      event.target.appendChild(document.getElementById(data));
+    drop(event) {
+      event.preventDefault()
+      var data = event.dataTransfer.getData("Text")
+      var element = document.getElementById(data)
+      var position = element.getElementsByClassName('position')[0].innerHTML
+
+      /* TODO work into an alert system */
+      if (event.target.id !== position) {
+        alert(`Player is a ${position}, can't place as a ${event.target.id}`)
+        return;
+      }
+
+      event.target.appendChild(element)
     }
   }
 }
